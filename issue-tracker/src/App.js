dc4130dc307e4f4b729a70174755b7e1959c3648
RicardoMonteiroSimoes <monteric@students.zhaw.ch>
2019-10-17 11:53:00 +0200

    Started working on the footer

--- issue-tracker/src/App.js
+++ issue-tracker/src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <Header appTitle="Yet another issue tracker" />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ issue-tracker/src/components/footer/footer.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from '../button/button.component';
+
+import './Footer.styles.css';
+
+const Footer = ({ myProperty }) => {;
+
+    return (
+      <footer>
+
+      </footer>
+    );
+};
+  
+  export default Footer;
