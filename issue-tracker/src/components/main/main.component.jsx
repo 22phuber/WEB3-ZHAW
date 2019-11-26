38975c92d86d194e62879917bb603b77bbf985f6
RicardoMonteiroSimoes <monteric@students.zhaw.ch>
2019-11-26 21:38:21 +0100

    Added floating speed dial for editing functions

--- issue-tracker/src/components/main/main.component.jsx
+++ issue-tracker/src/components/main/main.component.jsx
@@ -6,8 +6,13 @@ import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Fab from "@material-ui/core/Fab";
-import EditIcon from '@material-ui/icons/Edit';
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+
+import SpeedDial from '@material-ui/lab/SpeedDial';
+import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
+import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
 
 import "./main.styles.css";
 
@@ -23,6 +28,25 @@ const Main = props => {
 
   const [projectData, setProjectData] = useState(null);
   const [getProjectStatus, setGetProjectStatus] = useState(HerokuAPI.loadingState.waiting)
+  const [open, setOpen] = React.useState(false);
+  const [hidden, setHidden] = React.useState(false);
+
+  const actions = [
+    { icon: <AddIcon  />, name: 'New Project' },
+    { icon: <EditIcon />, name: 'New Issue' },
+  ];
+
+  const handleVisibility = () => {
+    setHidden(prevHidden => !prevHidden);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   function finishLoadingProjects(projectData) {
     setProjectData(projectData);
@@ -48,10 +72,10 @@ const Main = props => {
   },[projectData]);
 
   const useStyles = makeStyles(theme => ({
-    fab: {
+    speedDial: {
       position: 'absolute',
       bottom: theme.spacing(10),
-      right: theme.spacing(3),
+      right: theme.spacing(2),
     },
   }));
   const classes = useStyles();
@@ -60,9 +84,25 @@ const Main = props => {
     return (
       <div className="Main">
         <TabsPane data={projectData} />
-        <Fab aria-label={'Edit'} className={classes.fab} color={'primary'}>
-            <EditIcon />
-          </Fab>
+        <SpeedDial
+        ariaLabel="SpeedDial tooltip example"
+        className={classes.speedDial}
+        hidden={hidden}
+        icon={<SpeedDialIcon />}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+      >
+        {actions.map(action => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            tooltipOpen
+            onClick={handleClose}
+          />
+        ))}
+      </SpeedDial>
       </div>
     );
   } else if (getProjectStatus === HerokuAPI.loadingState.finished) {
