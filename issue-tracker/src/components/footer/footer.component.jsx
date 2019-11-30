1421deca90411f35c804354f8143bde02f8be0e4
RicardoMonteiroSimoes <monteric@students.zhaw.ch>
2019-11-30 17:00:53 +0100

    Updated footer to not show itself when on a mobile device

--- issue-tracker/src/components/footer/footer.component.jsx
+++ issue-tracker/src/components/footer/footer.component.jsx
@@ -4,22 +4,27 @@ import Switch from '@material-ui/core/Switch';
 
 import './footer.styles.css';
 
-const Footer = props => {;
-
+const Footer = props => {
+  if (props.mobileDevice) {
+    return (
+      null
+    );
+  } else {
     return (
       <footer className={"Footer".concat(props.darkMode ? " FooterDarkMode" : " FooterLightMode")}>
-        <FormControlLabel 
-            value="start"
-            control={<Switch 
-                      color="primary" 
-                      checked={props.checked || false}
-                      onChange={props.onChange} 
-                    />}
-            label="Nightmode"
-            labelPlacement="start"
-          />
+        <FormControlLabel
+          value="start"
+          control={<Switch
+            color="primary"
+            checked={props.checked || false}
+            onChange={props.onChange}
+          />}
+          label="Nightmode"
+          labelPlacement="start"
+        />
       </footer>
     );
+  }
 };
-  
-  export default Footer;
+
+export default Footer;
