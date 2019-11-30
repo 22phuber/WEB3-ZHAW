f6afe1bd2a019589785d9a17ceb3c018b0459a34
RicardoMonteiroSimoes <monteric@students.zhaw.ch>
2019-11-30 16:55:36 +0100

    Refactored darkMode function to use boolean

--- issue-tracker/src/App.js
+++ issue-tracker/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/header/header.component';
 
@@ -8,21 +8,21 @@ import Footer from './components/footer/footer.component';
 
 function App() {
 
-  const colourModeKey = "cmk";
-  const [colourMode, setColourMode] = useState(localStorage.getItem(colourModeKey)|| "-light");
+  const darkModeKey = "darkMode";
+  const [darkModeActive, setDarkMode] = useState(localStorage.getItem(darkModeKey) === 'true');
+  const [isMobileDevice, setIsModuleDevice] = useState(false);
+
+  if (/Mobi|Android/i.test(navigator.userAgent)) {
+    setIsModuleDevice(true);
+  }
 
   const changeColourMode = () => {
-    if(colourMode === "-light"){
-      setColourMode("-dark");
-    } else {
-      setColourMode("-light");
-    }
+    setDarkMode(!darkModeActive);
   }
 
   useEffect(() => {
-    localStorage.setItem(colourModeKey, colourMode);
-  }, [colourMode]);
-
+    localStorage.setItem(darkModeKey, darkModeActive);
+  }, [darkModeActive]);
 
   //Needed to block touch on iOS devices
   App.ontouchstart = (e) => {
@@ -31,9 +31,12 @@ function App() {
 
   return (
     <div className="App">
-      <Header appTitle="Yet another issue tracker" colourMode={colourMode}/>
-      <Main />
-      <Footer checked={colourMode === "-dark"} onChange={changeColourMode} colourMode={colourMode}/>
+      <Header appTitle="Yet another issue tracker" darkMode={darkModeActive} mobileDevice={isMobileDevice} />
+      <Main darkMode={darkModeActive} mobileDevice={isMobileDevice}/>
+      <Footer checked={darkModeActive} onChange={changeColourMode}
+        darkMode={darkModeActive}
+        mobileDevice={isMobileDevice} 
+      />
     </div>
   );
 }
