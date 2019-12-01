5accc18f902752f48af74b7a72571a3d04332b2b
RicardoMonteiroSimoes <monteric@students.zhaw.ch>
2019-12-01 19:06:58 +0100

    Added props for dynamic text in Loading component

--- src/components/loading/loading.component.jsx
+++ src/components/loading/loading.component.jsx
@@ -7,7 +7,7 @@ import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 const Loading = props => {
   return (
     <div className="Loading">
-      <h6>Fetching data...</h6>
+      <h6>{props.text}</h6>
       <span style={{color: "#3f51b5", opacity: 0.5}}>
         <FontAwesomeIcon icon={faSpinner} pulse />
       </span>
